72d91087436adee8e2080de54deb2bc8b9de4ad6
Angel Torres <alas4224@gmail.com>
2019-04-16 13:55:45 -0500

    setup styles folder and main files

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -7,7 +7,15 @@ class Nav extends Component {
             <nav>
                 {/* <img className="logo" src="" alt="logo"></img> */}
                 <div className="logo">Song Manager</div>
-                <div></div>
+                <div>
+                    <a>Blog</a>
+                    <a>Learn</a>
+                    <a>About Us</a>
+                </div>
+                <div className="CTA">
+                    <a>Sign up</a>
+                    <a>Log in</a>
+                </div>
             </nav>
         </div>
         )
