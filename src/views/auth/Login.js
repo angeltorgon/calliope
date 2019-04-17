46a669526c178dced215e0d516f5ab4d5d8806ea
Angel Torres <alas4224@gmail.com>
2019-04-16 22:28:29 -0500

    create login view

---
+++ src/views/auth/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Login() {
+    return (
+        <div className="form-container">
+            <form className="form">
+                <input type="text" placeholder="email/username"></input>
+                <input type="text" placeholder="password"></input>
+            </form>
+        </div>
+    )
+}
