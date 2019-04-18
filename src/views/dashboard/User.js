f9581bc03d689ba0490dcdfce8b2f43355dd98ed
Angel Torres <alas4224@gmail.com>
2019-04-18 16:06:16 -0500

    refactor User component

--- src/views/dashboard/User.js
+++ src/views/dashboard/User.js
@@ -3,9 +3,9 @@ import React, { useState } from 'react'
 export default function User() {
     const [name, setName] = useState('Angel Torres')
 
-  return (
-    <div>
-      <h1>{name}</h1>
-    </div>
-  )
+    return (
+        <div>
+            <h1>{name}</h1>
+        </div>
+    )
 }
