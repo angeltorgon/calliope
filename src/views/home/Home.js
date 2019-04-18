a4e5a95fbe8f450dac0fec0993e46020485da6c7
Angel Torres <alas4224@gmail.com>
2019-04-17 22:27:20 -0500

    add welcome text to home view

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -6,6 +6,7 @@ export default function Home() {
       <div className="banner">
         <img src="https://images.unsplash.com/photo-1516280440614-37939bbacd81?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="banner image"/>
       </div>
+      <h2 className="welcome-text">Welcome!</h2>
     </div>
   )
 }
