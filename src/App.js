0f40e00dfe7469f48dc417f3c0fd3210999a7354
Angel Torres <alas4224@gmail.com>
2019-04-17 22:00:07 -0500

    set up home component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import Nav from './components/nav/Nav';
 import Login from './views/auth/Login';
 import Signup from './views/auth/Signup';
+import Home from './views/home/Home';
 
 class App extends Component {
   render() {
     return (
       <div className="app-container">
         <Nav/>
+        <Route path="/" render={Home}/>
         <Route path="/login" render={Login}/>
         <Route path="/signup" render={Signup}/>
       </div>
