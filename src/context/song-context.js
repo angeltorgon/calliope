71c3b7f1503f6bc733e507d5e7b5f9276e19ac79
Angel Torres <alas4224@gmail.com>
2019-04-19 20:53:20 -0500

    set up context in Dashboard.js

--- src/context/song-context.js
+++ src/context/song-context.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default React.createContext({
     songs: [
-        {id:1, title:"en el nombre de jesus", artist:"christie checz" count:10},
+        {id:1, title:"en el nombre de jesus", artist:"christie checz", count:10},
         {id:2, title:"the stand", artist:"hillsong", count:10},
         {id:3, title:"abrenos los cielos", artist:"christine d'chlario", count:10},
         {id:4, title:"padre nuestro", artist:"bethel", count:10},
--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SongCard from './SongCard';
 
+import SongContext from '../../context/song-context';
+
 export default function Dashboard() {
+    const [ song, setSong ] = useState({
+        songs: [
+            {id:1, title:"en el nombre de jesus", artist:"christie checz", count:10},
+            {id:2, title:"the stand", artist:"hillsong", count:10},
+            {id:3, title:"abrenos los cielos", artist:"christine d'chlario", count:10},
+            {id:4, title:"padre nuestro", artist:"bethel", count:10},
+        ],
+        songList: []
+    });
+
+
     return (
-        <div>
-            <SongCard/>
-        </div>
+        <SongContext.Provider value={{
+            songs: song.songs,
+            songList: song.songList
+        }}>
+            <div>
+                <SongCard/>
+            </div>
+        </ SongContext.Provider>
   )
 }
