43ae06dcd220fe1a7bafa5f95b60589e21a1fcfb
Angel Torres <alas4224@gmail.com>
2019-04-20 23:25:28 -0500

    fix dashboard error

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -3,27 +3,19 @@ import SongCard from './SongCard';
 
 import SongContext from '../../context/song-context';
 
-export default function Dashboard() {
-    const [ song, setSong ] = useState({
-        songs: [
-            {id:1, title:"en el nombre de jesus", artist:"christie checz", count:10},
-            {id:2, title:"the stand", artist:"hillsong", count:10},
-            {id:3, title:"abrenos los cielos", artist:"christine d'chlario", count:10},
-            {id:4, title:"padre nuestro", artist:"bethel", count:10},
-        ],
-        songList: []
-    });
+function Dashboard() {
+    // const [ song, setSong ] = useState("hello");
 
-    const addSong = song => {console.log("Adding song..")};
+    // const addSong = song => {console.log("Adding song..")};
     
-    const deleteSong = songId => {console.log("Deleting song.. ")};
+    // const deleteSong = songId => {console.log("Deleting song.. ")};
 
     return (
         <SongContext.Provider value={{
-            songs: song.songs,
-            songList: song.songList,
-            addSong: addSong,
-            deleteSong: deleteSong
+            // songs: song.songs,
+            // songList: song.songList,
+            // addSong: addSong,
+            // deleteSong: deleteSong
         }}>
             <div>
                 <SongCard/>
@@ -31,3 +23,5 @@ export default function Dashboard() {
         </ SongContext.Provider>
   )
 }
+
+export default Dashboard;
