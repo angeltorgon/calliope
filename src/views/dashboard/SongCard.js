5ee61cfa30bccad5ffc5bc515da8dcf0ed94de89
Angel Torres <alas4224@gmail.com>
2019-04-22 11:38:25 -0500

    update styles on SongCard component

--- src/views/dashboard/SongCard.js
+++ src/views/dashboard/SongCard.js
@@ -4,9 +4,9 @@ import SongContext from '../../context/song-context';
 export default function SongCard() {
     return (
         <div className="song-card">
-            <h2 classNAme="card-title">Song Name</h2>
-            <h3 classNAme="card-artist" >by Artist</h3>
-            <p classNAme="card-title" >youtube link</p>
+            <h2 className="card-title">Song Name</h2>
+            <h3 className="card-artist" >by Artist</h3>
+            <p className="card-title" >youtube link</p>
         </div>
     )
 }
