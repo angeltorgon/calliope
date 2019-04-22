894a24f96521ded8481de1a302713872686afb6c
Angel Torres <alas4224@gmail.com>
2019-04-22 15:08:43 -0500

    brought in user context to Nav component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
+import UserContext from './context/user-context';
 import Nav from './components/nav/Nav';
 import Login from './views/auth/Login';
 import Signup from './views/auth/Signup';
@@ -10,13 +11,17 @@ import Dashboard from './views/dashboard/Dashboard';
 class App extends Component {
   render() {
     return (
-      <div className="app-container">
-        <Nav/>
-        <Route exact path="/" render={Home}/>
-        <Route path="/dashboard" render={Dashboard}/>
-        <Route path="/login" render={Login}/>
-        <Route path="/signup" render={Signup}/>
-      </div>
+        <UserContext.Provider value={{
+          loggedIn: false
+      }}>
+          <div className="app-container">
+            <Nav/>
+            <Route exact path="/" render={Home}/>
+            <Route path="/dashboard" render={Dashboard}/>
+            <Route path="/login" render={Login}/>
+            <Route path="/signup" render={Signup}/>
+          </div>
+      </UserContext.Provider>
     );
   }
 }
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,23 +1,28 @@
-import React, { Component } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
+import UserContext from '../../context/user-context';
 
-class Nav extends Component {
-    render() {
+function Nav() {
+        const context = useContext(UserContext);
+
+        useEffect(() => {
+            console.log(context)
+        })
+
         return (
-        <nav className="nav">
-                {/* <img className="logo" src="" alt="logo"></img> */}
-                <NavLink to="/" className="logo">Song Manager</NavLink>
-                <div className="link-container">
-                    <div className="link">Blog</div>
-                    <div className="link">Learn</div>
-                    <div className="link">About Us</div>
-                </div>
-                <div className="cta-container">
-                    <NavLink to="/signup" className="nav-cta signup">Sign up</NavLink>
-                    <NavLink to="/login" className="nav-cta login">Log in</NavLink>
-                </div>
-        </nav>
+                <nav className="nav">
+                        {/* <img className="logo" src="" alt="logo"></img> */}
+                        <NavLink to="/dashboard" className="logo">Song Manager</NavLink>
+                        <div className="link-container">
+                            <div className="link">Blog</div>
+                            <div className="link">Learn</div>
+                            <div className="link">About Us</div>
+                        </div>
+                        <div className="cta-container">
+                            <NavLink to="/signup" className="nav-cta signup">Sign up</NavLink>
+                            <NavLink to="/login" className="nav-cta login">Log in</NavLink>
+                        </div>
+                </nav>
         )
-    }
 }
 export default Nav;
