fc7764a33e7616bbacf3f027f446422c69129146
Angel Torres <alas4224@gmail.com>
2019-04-22 11:04:32 -0500

    add className to songcard tags

--- src/views/dashboard/SongCard.js
+++ src/views/dashboard/SongCard.js
@@ -4,9 +4,9 @@ import SongContext from '../../context/song-context';
 export default function SongCard() {
     return (
         <div className="song-card">
-            <h2>Song Name</h2>
-            <h3>by Artist</h3>
-            <p>youtube link</p>
+            <h2 classNAme="card-title">Song Name</h2>
+            <h3 classNAme="card-artist" >by Artist</h3>
+            <p classNAme="card-title" >youtube link</p>
         </div>
     )
 }
