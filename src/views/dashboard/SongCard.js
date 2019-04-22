acad4a852630682ec33c78cdbcbb9963db5998cf
Angel Torres <alas4224@gmail.com>
2019-04-22 14:27:29 -0500

    refactor SongCard component to use useState hook

--- src/views/dashboard/SongCard.js
+++ src/views/dashboard/SongCard.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 // import SongContext from '../../context/song-context';
 
 export default function SongCard(props) {
-    const [ song, setSong ] = useState("hello")
+    const [ song, setSong ] = useState(props.song.title);
+    const [ artist, setArtist ] = useState(props.song.artist);
+    const [ link, setLink ] = useState(props.song.youtubeLink);
     return (
         <div className="song-card">
-            <h2 className="card-title">{props.song.title}</h2>
-            <h2 className="card-artist" >{props.song.artist}</h2>
-            <a href={`${props.song.youtubeLink}`} className="card-title" >View on YouTube</a>
+            <h2 className="card-title">{song}</h2>
+            <h2 className="card-artist" >{artist}</h2>
+            <a href={`${link}`} className="card-title" >View on YouTube</a>
             <p>song</p>
         </div>
     )
