cadf8c6ceec600b1399cd65042e719cd9cc54719
angel-torres <alas4224@gmail.com>
2019-05-02 15:50:49 -0500

    add navbar functionality so that when user is logged in it displays different links than when user is logged out

--- src/views/layout/nav/Nav.js
+++ src/views/layout/nav/Nav.js
@@ -10,26 +10,37 @@ function Nav() {
     })
 
     return (
+        <>
+         {context.loggedIn ? 
+            <nav className="nav">
+                    {/* <img className="logo" src="" alt="logo"></img> */}
+                    <div to="/dashboard" className="logo">Calliope</div>
+                    <div className="link-container">
+                        <NavLink to="/dashboard" className="link">Dashboard</NavLink>
+                    </div>
+                    
+                    <div className="cta-container">
+                        <NavLink to="/profile" className="nav-cta login">Profile</NavLink>
+                        <NavLink to="/settings" className="nav-cta login">Settings</NavLink>
+                    </div>
+            </nav>
+        :
         <nav className="nav">
-                {/* <img className="logo" src="" alt="logo"></img> */}
-                <NavLink to="/dashboard" className="logo">Calliope</NavLink>
-                <div className="link-container">
-                    <div className="link">Blog</div>
-                    <div className="link">Learn</div>
-                    <div className="link">About Us</div>
-                </div>
-                {context.loggedIn ? 
-                <div className="cta-container">
-                    <NavLink to="/profile" className="nav-cta login">Profile</NavLink>
-                    <NavLink to="/settings" className="nav-cta login">Settings</NavLink>
-                </div>
-                : 
-                <div className="cta-container">
-                        <NavLink to="/signup" className="nav-cta signup">Sign up</NavLink>
-                        <NavLink to="/login" className="nav-cta login">Log in</NavLink>
-                </div>
-            }
+            {/* <img className="logo" src="" alt="logo"></img> */}
+            <NavLink to="/dashboard" className="logo">Calliope</NavLink>
+            <div className="link-container">
+                <div className="link">Blog</div>
+                <div className="link">Learn</div>
+                <div className="link">About Us</div>
+            </div>
+            
+            <div className="cta-container">
+                    <NavLink to="/signup" className="nav-cta signup">Sign up</NavLink>
+                    <NavLink to="/login" className="nav-cta login">Log in</NavLink>
+            </div>
         </nav>
+    }
+        </>
     )
 }
 export default Nav;
