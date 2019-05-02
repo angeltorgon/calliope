fea089376e9817a54ef3d9b16f0c20673631bb46
angel-torres <alas4224@gmail.com>
2019-05-02 15:29:56 -0500

    add changehandler and submit handler to signup component

--- src/views/auth/Signup.js
+++ src/views/auth/Signup.js
@@ -8,19 +8,20 @@ function Signup() {
     const [ email, setEmail ] = useState('');
     const [ password, setPassword ] = useState('');
 
-    const handleSubmit = function() {
-        console.log({})
+    const handleSubmit = function(e) {
+        e.preventDefault();
+        console.log({firstName, lastName, useState,email, password});
     }
 
     return (
         <div className="form-container">
             <h2>Sign up</h2>
-            <form className="form">
-                <input type="text" placeholder="first name"></input>
-                <input type="text" placeholder="last name"></input>
-                <input type="text" placeholder="username"></input>
-                <input type="text" placeholder="email"></input>
-                <input type="text" placeholder="password"></input>
+            <form className="form" onSubmit={handleSubmit}>
+                <input onChange={(e)=>{setFirstName(e.target.value)}} value={firstName} type="text" placeholder="first name"></input>
+                <input onChange={(e)=>{setLastName(e.target.value)}} value={lastName} type="text" placeholder="last name"></input>
+                <input onChange={(e)=>{setUsername(e.target.value)}} value={username} type="text" placeholder="username"></input>
+                <input onChange={(e)=>{setEmail(e.target.value)}} value={email} type="text" placeholder="email"></input>
+                <input onChange={(e)=>{setPassword(e.target.value)}} value={password} type="text" placeholder="password"></input>
                 <button>Sign up</button>
             </form>
         </div>
