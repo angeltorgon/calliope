dcadb78f90df60a84bda37bbfdf319a585f626a1
angel-torres <alas4224@gmail.com>
2019-05-02 15:10:04 -0500

    add state to login form

--- src/views/auth/Login.js
+++ src/views/auth/Login.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react';
 
-class Login extends Component {
-  render() {
-    return (
-      <div>
-        Hello
-      </div>
-    )
-  }
+export default function Login() {
+  
+  const [ email, setEmail ] = useState(''); 
+  const [ password, setPassword ] = useState(''); 
+
+
+
+  return (
+    <div className="form-container">
+    <h2>Sign up</h2>
+    <form className="form">
+        <input value={email} type="text" placeholder="email"></input>
+        <input value={password} type="text" placeholder="password"></input>
+        <button>Sign up</button>
+    </form>
+</div>
+  )
 }
-
-export default Login;
