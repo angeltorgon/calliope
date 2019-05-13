3d6cf358221ba02e40ab10abe6948c3330738f81
angel-torres <alas4224@gmail.com>
2019-05-13 17:22:09 -0500

    remove context folder

--- src/context/user-context.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default React.createContext({
-    firstName: 'stevon',
-    astName: 'martin',
-    userName: 'stevon',
-    email: 'stevon@gmail.com',
-    loggedIn: true,
-});
