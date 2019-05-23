252f0a3da0611e48536c47b01765c114ab0fb9c1
angel-torres <alas4224@gmail.com>
2019-05-23 15:22:08 -0500

    add profile picture and bio info to profile component

--- src/views/profile/Profile.js
+++ src/views/profile/Profile.js
@@ -5,7 +5,12 @@ function Profile(props) {
   console.log(props.user)
   return (
     <div className="profile-container">
-      hey
+      <img src="https://images.unsplash.com/photo-1476657680631-c07285ff2581?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1310&q=80" alt="user picture" alt="profile picture" />
+      <h2 className="user-name">{`${props.user.firstName} ${props.user.lastName}`}</h2>
+      <p className="user-bio">Bacon ipsum dolor amet short ribs brisket venison rump drumstick pig sausage prosciutto chicken spare ribs salami picanha doner. Kevin capicola sausage, buffalo bresaola venison turkey shoulder picanha ham pork tri-tip meatball meatloaf ribeye. Doner spare ribs andouille bacon sausage. Ground round jerky brisket pastrami shank.</p>
+      <div className="user-posts">
+        s
+      </div>
     </div>
   )
 }
