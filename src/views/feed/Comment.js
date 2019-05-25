5d76e69186925ee105a0938e4ce18c10d69cdb4a
angel-torres <alas4224@gmail.com>
2019-05-25 14:17:33 -0500

    refactor: trying to get username to appear for each comment

--- src/views/feed/Comment.js
+++ src/views/feed/Comment.js
@@ -4,11 +4,14 @@ import UserStamp from '../../components/UserStamp';
 
 export default function Comment(props) {
 
-  console.log(props);
-
   return (
     <div className="comment">
-      <p>{dateFormat(props.comment.created_at, 'mediumDate')}</p>
+      <UserStamp
+        userId={props.user_id}
+        poet={props.username}
+        date={dateFormat(props.updated_at, 'mediumDate')}
+      />
+   
       <p>{props.comment.comment}</p>
       <p>Likes: {props.comment.likes}</p>
     </div>
