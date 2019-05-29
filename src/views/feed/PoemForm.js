353a297303744af2ef6ef1cb7121be8fecdc6f74
angel-torres <alas4224@gmail.com>
2019-05-29 18:13:34 -0500

    almost done styling poem form

--- src/views/feed/PoemForm.js
+++ src/views/feed/PoemForm.js
@@ -6,10 +6,46 @@ class PoemForm extends React.Component {
     render() {
         return (
             <div className='poem-form'>
+                <h2>Write a Poem</h2>
                 <form>
-                    <input type='text' placeholder='title' />
-                    <input type='text' placeholder='title' />
+                    <input className="poem-title" type='text' placeholder='title' />
+                    <input type='text' placeholder='Write a poem' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
+                    <input type='text' placeholder='' />
                 </form>
+                <button>Add Poem</button>
             </div>
         );
     }
