7954b60e9a2af8399603008ce14be30de2cd4a14
angel-torres <alas4224@gmail.com>
2019-07-17 23:38:05 -0500

    added firebase and firebaseui as dependencies

--- src/hooks/index.js
+++
@@ -1,7 +0,0 @@
-export const useFeedRedirect = (props) => {
-    if(localStorage.getItem('token')) {
-        return props.history.push('/dashboard');
-      } else {
-        return props.history.push('/');
-      };
-};
