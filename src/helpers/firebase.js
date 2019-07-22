9f43f9560f98c14ba20b4efc82459c5d830227eb
angel-torres <alas4224@gmail.com>
2019-07-22 09:41:26 -0500

    add firebase config to helpers/firebasee.js

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -1,3 +1,17 @@
 import firebase from "firebase";
-import firebaseui from "firebaseui";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAWA4BH7aoYX1-lwUPaNl9xfKCk82Fqv48",
+    authDomain: "fir-tutorial-cad7d.firebaseapp.com",
+    databaseURL: "https://fir-tutorial-cad7d.firebaseio.com",
+    projectId: "fir-tutorial-cad7d",
+    storageBucket: "fir-tutorial-cad7d.appspot.com",
+    messagingSenderId: "976833235192",
+    appId: "1:976833235192:web:ee123d5777eb0ff2"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
 
