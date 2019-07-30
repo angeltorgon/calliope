2b9e6617c53bf3e4c78f5b8e107309bd351a83e8
angel-torres <alas4224@gmail.com>
2019-07-29 22:13:53 -0500

    update actions and reducers folder to include signupActions.js and signupReducer.js; still refactoring redux in app

---
+++ src/store/actions/signupActions.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+import firebase from "../../firebase";
+
+export const SIGN_UP_START = "SIGN_UP";
+export const SIGN_UP_SUCCESS = "SIGN_UP_SUCCESS";
+export const SIGN_UP_FAILURE = "SIGN_UP_FAILURE";
+
+var provider = new firebase.auth.GoogleAuthProvider();
+
+export const signup = () => {
+    dispatch({type: SIGN_UP_START})
+    firebase
+        .auth()
+        .signInWithRedirect(provider);
+};
