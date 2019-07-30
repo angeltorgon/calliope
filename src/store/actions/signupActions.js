b648fdf1504b090f48ac97cdafe086261b5fbfdb
angel-torres <alas4224@gmail.com>
2019-07-29 23:28:22 -0500

    add functionality so that loader does not display once start user is logged in; still working on getting api request to be sent once user is signed up

--- src/store/actions/signupActions.js
+++ src/store/actions/signupActions.js
@@ -8,8 +8,29 @@ export const SIGN_UP_FAILURE = "SIGN_UP_FAILURE";
 var provider = new firebase.auth.GoogleAuthProvider();
 
 export const signup = () => dispatch => {
-    dispatch({type: SIGN_UP_START})
+    dispatch({ type: SIGN_UP_START });
     firebase
         .auth()
-        .signInWithRedirect(provider);
+        .signInWithPopup(provider)
+        .then(function(result) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            // ...
+            console.log("token", token);
+            console.log("user", user);
+            dispatch({ type: SIGN_UP_SUCCESS })
+        })
+        .catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+            dispatch({ type: SIGN_UP_FAILURE });
+        });
 };
