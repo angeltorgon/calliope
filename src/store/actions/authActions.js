2b0f7f2d042a66e130c9bf0b21118cd909aa1608
angel-torres <alas4224@gmail.com>
2019-08-06 23:07:26 -0500

    auth is working; better error handling needs to be implemented

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -12,23 +12,16 @@ export const authWithGoogle = () => dispatch => {
         .auth()
         .signInWithPopup(provider)
         .then(function(result) {
-            // This gives you a Google Access Token. You can use it to access the Google API.
             var token = result.credential.accessToken;
-            // The signed-in user info.
             var user = result.user;
-            // ...
             localStorage.setItem("token", user.ra);
             dispatch({ type: SIGN_UP_SUCCESS });
         })
         .catch(function(error) {
-            // Handle Errors here.
             var errorCode = error.code;
             var errorMessage = error.message;
-            // The email of the user's account used.
             var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
             var credential = error.credential;
-            // ...
             dispatch({ type: SIGN_UP_FAILURE });
         });
 };
