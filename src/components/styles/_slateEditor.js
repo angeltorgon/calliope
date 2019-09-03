209d7e28763fe1297654d2fee455a947324eb44e
angel-torres <alas4224@gmail.com>
2019-09-02 23:15:26 -0500

    added container and padding to the editor

--- src/components/styles/_slateEditor.js
+++ src/components/styles/_slateEditor.js
@@ -2,7 +2,14 @@ import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
     editorContainer: {
-        border: "1px solid red",
+        border: "1px solid lightgrey",
+        borderRadius: "10px",
+        width: "100%",
+        maxWidth: "800px",
+        maxHeight: "500px",
+        height: "100%",
+        padding: "40px",
+        margin: "60px 20px"
     }
 }));
 
