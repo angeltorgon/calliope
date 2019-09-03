6f23cf94f02c611cc72b9cdbb8898b0f5513f8fb
angel-torres <alas4224@gmail.com>
2019-09-03 17:14:26 -0500

    added functionality to go back in write viw

--- src/components/WriteNavbar.js
+++ src/components/WriteNavbar.js
@@ -9,22 +9,20 @@ import useStyles from "./styles/_writeNavbar";
 function Navbar(props) {
     const classes = useStyles();
 
-    const handleLogout = e => {
-        Firebase.signout();
-        localStorage.removeItem("token");
-        props.history.push("/");
+    const goBack = e => {
+        props.history.goBack()
     };
 
     return (
         <>
             {props.user ? (
                 <div className={classes.nav}>
-                    <Link to="/" >
+                    <div onClick={goBack}>
                         <svg className={classes.backButton} xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
                             width="48" height="48"
                             viewBox="0 0 172 172"
                             style={{ fill: "#000000" }}><g fill="none" fillRule="nonzero" stroke="none" strokeWidth="1" strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="10" strokeDasharray="" strokeDashoffset="0" fontFamily="none" fontWeight="none" fontSize="none" textAnchor="none" style={{ mixBlendMode: "normal" }}><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#de6998"><g id="surface1"><path d="M116.45833,9.63021l-64.5,71.66667l-4.25521,4.70313l4.25521,4.70313l64.5,71.66667l10.75,-9.40625l-60.24479,-66.96354l60.24479,-66.96354z"></path></g></g></g></svg>
-                    </Link>
+                    </div>
                     <div className={classes.linkContainer}>
                         {/* <Link
                             to="/"
