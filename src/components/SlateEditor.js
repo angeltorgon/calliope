64bd3051efed99ff66ac6e9bdb4b2bde82ca8e3f
angel-torres <alas4224@gmail.com>
2019-09-08 00:28:46 -0500

    added TitleNode component to slate editor component; still playing with slate.js

--- src/components/SlateEditor.js
+++ src/components/SlateEditor.js
@@ -5,89 +5,95 @@ import { Value } from 'slate'
 import useStyles from './styles/_slateEditor';
 
 const initialValue = Value.fromJSON({
-    document: {
-        nodes: [
-            {
-                object: 'block',
-                type: 'title',
-                nodes: [
-                    {
-                        object: 'text',
-                        text:"Title"
-                    },
-                ],
-            },
-            {
-                object: 'block',
-                type: 'paragraph',
-                nodes: [
-                    {
-                        object: 'text',
-                        text:"Paragraph"
-                    },
-                ],
-            },
-        ],
-    },
-})
-
-const schema = {
-    document: {
-      nodes: [
-        {
-          match: [{ type: 'paragraph' }],
-        },
-      ],
-    },
-    blocks: {
-      paragraph: {
+  document: {
+    nodes: [
+      {
+        object: 'block',
+        type: 'title',
         nodes: [
           {
-            match: { object: 'text' },
+            object: 'text',
+            text: "Title"
           },
         ],
       },
-    },
-  }
+      {
+        object: 'block',
+        type: 'paragraph',
+        nodes: [
+          {
+            object: 'text',
+            text: "Paragraph"
+          },
+        ],
+      },
+    ],
+  },
+})
 
-
+const schema = {
+  document: {
+    nodes: [
+      {
+        object: 'block',
+        type: 'title',
+        nodes: [
+          {
+            object: 'text',
+            text: "Title"
+          },
+        ],
+      },
+      {
+        object: 'block',
+        type: 'paragraph',
+        nodes: [
+          {
+            object: 'text',
+            text: "Paragraph"
+          },
+        ],
+      },
+    ],
+  },
+}
 
 // Define our app...
 const SlateEditor = () => {
-    const classes = useStyles();
-    // Set the initial value when the app is first constructed.
-    const [state, setState] = useState({
-        value: initialValue,
-    });
+  const classes = useStyles();
+  // Set the initial value when the app is first constructed.
+  const [state, setState] = useState({
+    value: initialValue,
+  });
 
-    // On change, update the app's React state with the new editor value.
-    const onChange = ({ value }) => {
-        setState({ value });
-    }
+  // On change, update the app's React state with the new editor value.
+  const onChange = ({ value }) => {
+    setState({ value });
+  }
 
-    const renderTitle = (props) => {
-
-    }
-
-    // Render the editor.
-    return (
-        <div className={classes.editorContainer} >
-            <Editor schema={schema} className={classes.editor} value={state.value} onChange={onChange}  placeholder="Write your poetry..." />
-        </div>
-    )
+  // Render the editor.
+  return (
+    <div className={classes.editorContainer} >
+      <Editor renderBlock={renderBlock} schema={schema} className={classes.editor} value={state.value} onChange={onChange} placeholder="Write your poetry..." />
+    </div>
+  )
 }
 
+const TitleNode = props => {
+  return <h1>{props.children}</h1>;
+};
+
 const renderBlock = (props, editor, next) => {
-    const { attributes, children, node } = props
-    switch (node.type) {
-      case 'title':
-        return <h2 {...attributes}>{children}</h2>
-      case 'paragraph':
-        return <p {...attributes}>{children}</p>
-      default:
-        return next()
-    }
+  const { attributes, children, node } = props
+  switch (node.type) {
+    case 'title':
+      return <TitleNode {...attributes}>{children}</TitleNode>
+    case 'paragraph':
+      return <span {...attributes}>{children}</span>
+    default:
+      return next()
   }
+}
 
 
 export default SlateEditor;
