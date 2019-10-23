ab985c9b63ea06a12869cfd68d2b4c29b01ab3de
angel-torres <alas4224@gmail.com>
2019-10-23 11:26:37 -0500

    updated navbar logo to font-awesome logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import { makeStyles } from "@material-ui/core/styles";
 import { authWithGoogle } from "../store/actions";
 import { withRouter } from "react-router-dom";
 import Firebase from "../firebase";
-import useStyles from "./styles/_navbar"
+import useStyles from "./styles/_navbar";
+import colors from "../helpers/colorPalette";
 
 function Navbar(props) {
     const classes = useStyles();
@@ -22,8 +24,6 @@ function Navbar(props) {
                 <div className={classes.nav}>
                     <Link to="/" >
                         <img className={classes.logo} src={require("../images/calliope-logo.png")}></img>
-
-
                     </Link>
                     <div className={classes.linkContainer}>
                         <Link
@@ -31,11 +31,7 @@ function Navbar(props) {
                             className={classes.linkPink}
                             onClick={handleLogout}
                         >
-                            {/* <img className={classes.logout} src="https://img.icons8.com/ios/50/000000/exit.png"></img> */}
-                            <svg className={classes.logout} xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                                width="50" height="50"
-                                viewBox="0 0 50 50"
-                                style={{ fill: "#DA6991" }}>    <path style={{ lineHeight: "normal", textIndent: 0, textAlign: "start", textDecorationLine: "none", textDecorationStyle: "solid", textDecorationColor: "#FFDAD8", textTransform: "none", blockProgression: "tb", isolation: "auto", mixBlendMode: "normal" }} d="M 25 2 C 12.309534 2 2 12.309534 2 25 C 2 37.690466 12.309534 48 25 48 C 32.610275 48 39.366605 44.294736 43.550781 38.591797 A 1.0001 1.0001 0 1 0 41.939453 37.408203 C 38.117629 42.617264 31.961725 46 25 46 C 13.390466 46 4 36.609534 4 25 C 4 13.390466 13.390466 4 25 4 C 31.961725 4 38.117629 7.3827357 41.939453 12.591797 A 1.0001 1.0001 0 1 0 43.550781 11.408203 C 39.366605 5.7052643 32.610275 2 25 2 z M 38.990234 15.990234 A 1.0001 1.0001 0 0 0 38.292969 17.707031 L 44.585938 24 L 23 24 A 1.0001 1.0001 0 1 0 23 26 L 44.585938 26 L 38.292969 32.292969 A 1.0001 1.0001 0 1 0 39.707031 33.707031 L 47.619141 25.794922 A 1.0001 1.0001 0 0 0 47.617188 24.203125 L 39.707031 16.292969 A 1.0001 1.0001 0 0 0 38.990234 15.990234 z" font-weight="400" font-family="sans-serif" white-space="normal" overflow="visible"></path></svg>
+                            <FontAwesomeIcon icon={faSignOutAlt} size="2x" color={colors.primary} />
                         </Link>
                     </div>
                 </div>
