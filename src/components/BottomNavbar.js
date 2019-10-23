92cfd12543c958ce793d719041ad55f4c3bc5119
angel-torres <alas4224@gmail.com>
2019-10-23 11:35:28 -0500

    updated icons and icon colors for bottom nav. The colors will now be coming from the colors palette

--- src/components/BottomNavbar.js
+++ src/components/BottomNavbar.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import { makeStyles } from "@material-ui/core/styles";
+import { faSearch, faPlusCircle, faHome, faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import colors from "../helpers/colorPalette";
 import { authWithGoogle } from "../store/actions";
 import { withRouter } from "react-router-dom";
 import Firebase from "../firebase";
@@ -21,35 +23,16 @@ function Navbar(props) {
             {props.user ? (
                 <div className={classes.nav}>
                     <Link to="/search" className={classes.linkPink} >
-                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                            width="40" height="40"
-                            viewBox="0 0 172 172"
-                            style={{ fill: "#000000" }}><g fill="none" fillRule="nonzero" stroke="none" strokeWidth="1" strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="10" strokeDasharray="" strokeDashoffset="0" fontFamily="none" fontWeight="none" fontSize="none" textAnchor="none" style={{ mixBlendMode: "normal" }}><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#de6998"><g id="surface1"><path d="M72.24,10.32c-32.26344,0 -58.48,26.21656 -58.48,58.48c0,32.26344 26.21656,58.48 58.48,58.48c12.76563,0 24.56375,-4.11187 34.185,-11.0725l45.2575,45.15l9.675,-9.675l-44.72,-44.8275c8.78813,-10.23937 14.0825,-23.52906 14.0825,-38.055c0,-32.26344 -26.21656,-58.48 -58.48,-58.48zM72.24,17.2c28.54125,0 51.6,23.05875 51.6,51.6c0,28.54125 -23.05875,51.6 -51.6,51.6c-28.54125,0 -51.6,-23.05875 -51.6,-51.6c0,-28.54125 23.05875,-51.6 51.6,-51.6z"></path></g></g></g></svg>
+                        <FontAwesomeIcon icon={faSearch} size="2x" color={colors.primary} />
                     </Link>
-
                     <Link to="/write" className={classes.linkPink}>
-                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                            width="40" height="40"
-                            viewBox="0 0 172 172"
-                            style={{ fill: "#000000" }}><g fill="none" fillRule="nonzero" stroke="none" strokeWidth="1" strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="10" strokeDasharray="" strokeDashoffset="0" fontFamily="none" fontWeight="none" fontSize="none" textAnchor="none" style={{ mixBlendMode: "normal" }}><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#de6998"><path d="M86,6.88c-43.65603,0 -79.12,35.46397 -79.12,79.12c0,43.65603 35.46397,79.12 79.12,79.12c43.65603,0 79.12,-35.46397 79.12,-79.12c0,-43.65603 -35.46397,-79.12 -79.12,-79.12zM86,13.76c39.93779,0 72.24,32.30221 72.24,72.24c0,39.93779 -32.30221,72.24 -72.24,72.24c-39.93779,0 -72.24,-32.30221 -72.24,-72.24c0,-39.93779 32.30221,-72.24 72.24,-72.24zM85.94625,58.43297c-1.89722,0.02966 -3.41223,1.58976 -3.38625,3.48703v20.64h-20.64c-1.24059,-0.01754 -2.39452,0.63425 -3.01993,1.7058c-0.62541,1.07155 -0.62541,2.39684 0,3.46839c0.62541,1.07155 1.77935,1.72335 3.01993,1.7058h20.64v20.64c-0.01754,1.24059 0.63425,2.39452 1.7058,3.01993c1.07155,0.62541 2.39684,0.62541 3.46839,0c1.07155,-0.62541 1.72335,-1.77935 1.7058,-3.01993v-20.64h20.64c1.24059,0.01754 2.39452,-0.63425 3.01993,-1.7058c0.62541,-1.07155 0.62541,-2.39684 0,-3.46839c-0.62541,-1.07155 -1.77935,-1.72335 -3.01993,-1.7058h-20.64v-20.64c0.01273,-0.92983 -0.35149,-1.82522 -1.00967,-2.48214c-0.65819,-0.65692 -1.55427,-1.01942 -2.48408,-1.00489z"></path></g></g></svg>
+                        <FontAwesomeIcon icon={faPlusCircle} size="2x" color={colors.primary} />
                     </Link>
-                    <Link
-                        to="/home"
-                        className={classes.linkPink}
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                            width="40" height="40"
-                            viewBox="0 0 172 172"
-                            style={{ fill: "#000000" }}><g fill="none" fillRule="nonzero" stroke="none" strokeWidth="1" strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="10" strokeDasharray="" strokeDashoffset="0" fontFamily="none" fontWeight="none" fontSize="none" textAnchor="none" style={{ mixBlendMode: "normal" }}><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#de6998"><path d="M85.87235,3.62813c-0.72195,0.0249 -1.41775,0.27642 -1.98875,0.71891l-79.12,61.7386c-1.49911,1.16886 -1.76683,3.33167 -0.59797,4.83078c1.16886,1.49911 3.33167,1.76683 4.83078,0.59797l4.76359,-3.71547v90.4411c0.00019,1.89978 1.54022,3.43981 3.44,3.44h47.58219c0.37149,0.0614 0.75054,0.0614 1.12203,0h40.15797c0.37149,0.0614 0.75054,0.0614 1.12203,0h47.61578c1.89978,-0.00019 3.43981,-1.54022 3.44,-3.44v-90.4411l4.7636,3.71547c0.96974,0.75623 2.26928,0.93763 3.40904,0.47586c1.13976,-0.46177 1.94657,-1.49654 2.11649,-2.71449c0.16992,-1.21795 -0.32287,-2.43403 -1.29273,-3.19012l-26.1964,-20.43844v-25.00719h-20.64v8.89562l-32.2836,-25.18859c-0.64007,-0.49632 -1.43474,-0.7509 -2.24406,-0.71891zM86,11.42859l65.36,51.00203v92.36937h-41.28v-65.36h-48.16v65.36h-41.28v-92.36937zM127.28,27.52h6.88v12.7589l-6.88,-5.375zM68.8,96.32h34.4v58.48h-34.4z"></path></g></g></svg>
+                    <Link to="/home" className={classes.linkPink} >
+                        <FontAwesomeIcon icon={faHome} size="2x" color={colors.primary} />
                     </Link>
-                    <Link
-                        to="/profile"
-                        className={classes.linkPink}
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                            width="48" height="48"
-                            viewBox="0 0 172 172"
-                            style={{ fill: "#000000" }}><g transform="translate(5.504,5.504) scale(0.936,0.936)"><g fill="none" fillRule="nonzero" stroke="none" strokeWidth="none" strokeLinecap="butt" strokeLinejoin="none" strokeMiterlimit="10" strokeDasharray="" strokeDashoffset="0" fontFamily="none" fontWeight="none" fontSize="none" textAnchor="none" style={{ mixBlendMode: "normal" }}><g fill="#ffffff" stroke="#da6991" strokeWidth="14" strokeLinejoin="round"><path d="M114.66667,50.16667c0,15.83216 -12.8345,28.66667 -28.66667,28.66667c-15.83216,0 -28.66667,-12.8345 -28.66667,-28.66667c0,-15.83216 12.8345,-28.66667 28.66667,-28.66667c15.83216,0 28.66667,12.8345 28.66667,28.66667zM150.5,132.58333v10.75c0,3.956 -3.21067,7.16667 -7.16667,7.16667h-114.66667c-3.956,0 -7.16667,-3.21067 -7.16667,-7.16667v-10.75c0,-21.44267 42.97133,-32.25 64.5,-32.25c21.52867,0 64.5,10.80733 64.5,32.25z"></path></g><path d="M0,172v-172h172v172z" fill="none" stroke="none" strokeWidth="1" stroke-linejoin="miter"></path><g fill="#ffffff" stroke="none" strokeWidth="1" strokeLinejoin="miter"><path d="M86,21.5c-15.83216,0 -28.66667,12.8345 -28.66667,28.66667c0,15.83216 12.8345,28.66667 28.66667,28.66667c15.83216,0 28.66667,-12.8345 28.66667,-28.66667c0,-15.83216 -12.8345,-28.66667 -28.66667,-28.66667zM86,100.33333c-21.52867,0 -64.5,10.80733 -64.5,32.25v10.75c0,3.956 3.21067,7.16667 7.16667,7.16667h114.66667c3.956,0 7.16667,-3.21067 7.16667,-7.16667v-10.75c0,-21.44267 -42.97133,-32.25 -64.5,-32.25z"></path></g><path d="" fill="none" stroke="none" strokeWidth="1" strokeLinejoin="miter"></path><path d="" fill="none" stroke="none" strokeWidth="1" strokeLinejoin="miter"></path><path d="" fill="none" stroke="none" strokeWidth="1" strokeLinejoin="miter"></path></g></g></svg>
+                    <Link to="/profile" className={classes.linkPink}>
+                        <FontAwesomeIcon icon={faUser} size="2x" color={colors.primary} />
                     </Link>
                 </div>
             ) : null}</>
