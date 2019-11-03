87d1198fd9a338975b1edb8834771fd2497543df
angel-torres <alas4224@gmail.com>
2019-11-02 23:55:21 -0500

    created a UserPosts component to render the posts the user has posted

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Switch } from "react-router-dom";
 import { connect } from "react-redux";
 import Firebase from "./firebase";
 import { loggedIn, loggedOut, checkingUser } from './store/actions';
