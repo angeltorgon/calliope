1a70f397736102a80e5ea02daad722eac8db03aa
angel-torres <alas4224@gmail.com>
2019-11-10 22:54:46 -0600

    added ProfileImage component to settings view

--- src/views/Settings.js
+++ src/views/Settings.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import SettingsNavbar from '../components/SettingsNavbar'
+import ProfileImage from '../components/ProfileImage'
 
 export default function Settings() {
     return (
         <div>
-            <h1>Settings</h1>
             <SettingsNavbar />
+            <ProfileImage />
         </div>
     )
 }
