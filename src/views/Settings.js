fdc3d816e2b752e3bab25db681a06b6fca350b1a
angel-torres <alas4224@gmail.com>
2019-11-10 23:09:39 -0600

    added a few styles to _settings.js; working on centering image and adding more content

--- src/views/Settings.js
+++ src/views/Settings.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import useStyles from './styles/_settings'
 import SettingsNavbar from '../components/SettingsNavbar'
 import ProfileImage from '../components/ProfileImage'
 
 export default function Settings() {
+    const classes = useStyles();
     return (
         <div>
             <SettingsNavbar />
-            <ProfileImage />
+            <div className={classes.settingsContainer}>
+                <ProfileImage />
+            </div>
         </div>
     )
 }
---
+++ src/views/styles/_settings.js
@@ -0,0 +1,13 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+    settingsContainer: {
+        border: "1px solid red",
+        margin: "0 auto",
+        marginTop: "60px",
+        maxWidth: "1000px",
+        minHeight: "500px"
+    }
+}));
+
+export default useStyles;
