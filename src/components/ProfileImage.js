b3790f6d2dbf6bab868c638f90618538c691121e
angel-torres <alas4224@gmail.com>
2019-11-22 23:53:54 -0600

    made profileImage component more reusable by taking out the fixed positioning

--- src/components/ProfileImage.js
+++ src/components/ProfileImage.js
@@ -5,8 +5,6 @@ export default function ProfileImg() {
     const classes = useStyles();
 
     return (
-        <div className={classes.profileImageContainer} >
-            <img className={classes.profileImage} src="https://images.unsplash.com/photo-1531123897727-8f129e1688ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" />
-        </div>
+        <img className={classes.profileImage} alt="user profile" src="https://images.unsplash.com/photo-1531123897727-8f129e1688ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" />
     )
 }
--- src/components/styles/_profileImage.js
+++ src/components/styles/_profileImage.js
@@ -1,20 +1,11 @@
 import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
-    profileImageContainer: {
-        maxWidth: "1000px",
-        width: "100%",
-        margin: "0 auto",
-        marginBottom: "60px",
-        position: "relative",
-    },
     profileImage: {
-        position: "absolute",
         height: "120px",
         width: "120px",
         boxShadow: "0 1px 3px 2px rgba(5, 5, 5, .3)",
         borderRadius: "50%",
-        top: "-60px",
         border: "2px solid white"
     }
 }));
